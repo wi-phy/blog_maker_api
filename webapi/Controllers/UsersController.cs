0504805c00cd135f8cb5b3c1c62bef48cb2ae58f
wiphy <lm802085@gmail.com>
2025-01-22 23:25:51 +0100

    feat: add users controller

---
+++ webapi/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using webapi.Data;
+using webapi.Entities;
+
+namespace webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = await context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await context.Users.FindAsync(id);
+
+            if (user == null) return NotFound();
+
+            return user;
+        }
+    }
+}
