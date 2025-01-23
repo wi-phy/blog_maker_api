dfb316c24a02d9ea06d891ecda4994bd651b617a
wiphy <lm802085@gmail.com>
2025-01-23 01:59:31 +0100

    feat: add migrations

---
+++ webapi/Data/Migrations/20250123005433_AddGeneratedTime.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace webapi.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGeneratedTime : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
